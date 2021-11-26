ddd273f26b57447df0abef1c4bc76bd2211c85d8
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2021-11-26 12:15:19 +0300

    API updated

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,3 @@
-// Option 1
-
-// import { createServer } from "http";
-// import { Socket, Server } from "socket.io";
-
-// const httpServer = createServer();
-// const io = new Server(httpServer, { cors: { origin: "http://localhost:5000" } });
-
-// io.on("connection", (socket: Socket) => {
-//   console.log("connected to server", socket.id, new Date());
-// });
-
-// httpServer.listen(3000);
-
-// Option 2
 import express from "express";
 import http from "http";
 import socketio from "socket.io";
